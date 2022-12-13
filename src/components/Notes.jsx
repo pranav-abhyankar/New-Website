7b22d9c3c0d3816439c742a0dbad4e820e3af777
Pranav Abhyankar <114946272+pranav-abhyankar@users.noreply.github.com>
2022-12-13 22:20:03 +0530

    Initial commit

---
+++ src/components/Notes.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>Cyber Security</h1>
+      <p>
+        Cyber security is the practice of defending computers, servers, mobile
+        devices, electronic systems, networks, and data from malicious attacks.
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
